83502641b9eef54cd28f6945cfaef18fbd971d26
spex130 <lsmith0093@gmail.com>
2026-01-31 16:01:07 -0500

    Player Animations and State Machine implemented

--- Scripts/PlayerChar.gd
+++ Scripts/PlayerChar.gd
@@ -3,10 +3,14 @@ class_name PlayerChar extends Node3D
 @export var ParentArena : Arena 
 @export var Velocity : float = 0
 @export var WalkSpeed : float = .085
-@export var StopSpeed = .2
+@export var StopSpeed = .4
+@export var SlowSpeed = .1
 @export var MaxWalkSpeed : float = 1
 @export var IsMovingLeft : bool = false
-@export var PlayerAnimator : AnimationPlayer
+@export var PlayerAnimator : AnimationPlayer 
+var ExpectedRotation = 0
+enum PlayerState {Entry, Idle, Run, ChargeL, ChargeR, SwingL, SwingR}
+@export var CharacterState : PlayerState = PlayerState.Idle
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,15 +20,109 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_right"):
-		Velocity = min(MakePositive(Velocity) + WalkSpeed, MaxWalkSpeed)
-	elif Input.is_action_pressed("move_left"):
-		Velocity = max(MakeNegative(Velocity) -WalkSpeed, MakeNegative(MaxWalkSpeed))
-	else:
-		Velocity = Lerp(Velocity, 0, StopSpeed)
-	position.x += Velocity
+	StateMachineCheck()
 	WallCheck()
 	pass
+
+func ChangeState(state: PlayerState) -> void:
+	CharacterState = state
+	match(CharacterState):
+		PlayerState.Idle:
+			SetExpectedRotation(0)
+			PlayerAnimator.play("CupidIdle")
+		PlayerState.Run:
+			if(IsMovingLeft):
+				SetExpectedRotation(90)
+			else:
+				SetExpectedRotation(-90)
+			PlayerAnimator.play("CupidRun")
+		PlayerState.ChargeL:
+			SetExpectedRotation(0)
+			PlayerAnimator.play("CupidChargeL")
+		PlayerState.ChargeR:
+			SetExpectedRotation(0)
+
+			PlayerAnimator.play("CupidChargeR")
+		PlayerState.SwingL:
+			SetExpectedRotation(0)
+
+			PlayerAnimator.play("SwingL")
+		PlayerState.SwingR:
+			SetExpectedRotation(0)
+
+			PlayerAnimator.play("SwingR")
+	pass
+
+func StateMachineCheck() -> void:
+	rotation.y = ThresholdLerp(rotation.y, ExpectedRotation, .5, .1)
+	match(CharacterState):
+	
+		PlayerState.Idle:
+			Velocity = Lerp(Velocity, 0, StopSpeed)
+			
+			if Input.is_action_pressed("move_right"):
+				IsMovingLeft = false
+				ChangeState(PlayerState.Run)
+			elif Input.is_action_pressed("move_left"):
+				IsMovingLeft = true
+				ChangeState(PlayerState.Run)
+			elif Input.is_action_pressed("accept"):
+				if(IsMovingLeft):
+					ChangeState(PlayerState.ChargeL)
+				else:
+					ChangeState(PlayerState.ChargeR)
+				
+		PlayerState.Run:
+			if IsMovingLeft == false:
+				Velocity = min(MakePositive(Velocity) + WalkSpeed, MaxWalkSpeed)
+			elif IsMovingLeft == true:
+				Velocity = max(MakeNegative(Velocity) -WalkSpeed, MakeNegative(MaxWalkSpeed))
+			
+			if Input.is_action_pressed("accept"):
+				if(IsMovingLeft):
+					ChangeState(PlayerState.ChargeL)
+				else:
+					ChangeState(PlayerState.ChargeR)
+			
+			if Input.is_action_just_released("move_right") || Input.is_action_just_released("move_left"):
+				ChangeState(PlayerState.Idle)
+
+		PlayerState.ChargeL:
+			Velocity = Lerp(Velocity, 0, SlowSpeed)
+			if Input.is_action_just_released("accept"):
+				ChangeState(PlayerState.SwingL)
+		PlayerState.ChargeR:
+			Velocity = Lerp(Velocity, 0, SlowSpeed)
+			if Input.is_action_just_released("accept"):
+				ChangeState(PlayerState.SwingR)
+		PlayerState.SwingL:
+			Velocity = Lerp(Velocity, 0, StopSpeed)
+			if Input.is_action_pressed("accept") && PlayerAnimator.current_animation_position > .2:
+				if(IsMovingLeft):
+					ChangeState(PlayerState.ChargeL)
+				else:
+					ChangeState(PlayerState.ChargeR)
+			SwingIASA()
+		PlayerState.SwingR:
+			Velocity = Lerp(Velocity, 0, StopSpeed)
+			if Input.is_action_pressed("accept") && PlayerAnimator.current_animation_position > .2:
+				if(IsMovingLeft):
+					ChangeState(PlayerState.ChargeL)
+				else:
+					ChangeState(PlayerState.ChargeR)
+			SwingIASA()
+	
+	position.x += Velocity
+	pass
+
+func SwingIASA() -> void:
+	if PlayerAnimator.current_animation_position > .5:
+		if Input.is_action_pressed("move_right"):
+			IsMovingLeft = false
+			ChangeState(PlayerState.Run)
+		elif Input.is_action_pressed("move_left"):
+			IsMovingLeft = true
+			ChangeState(PlayerState.Run)
 
 func WallCheck() -> void:
 	var CalcSize = 1
@@ -50,6 +148,25 @@ func MakeNegative(number: float) -> float:
 	return abs(number) * -1
 	pass	
 
+func SetExpectedRotation(degrees: float) -> void:
+	ExpectedRotation = degrees * 0.01745329 #Convert to Radians
+	pass
+
 func Lerp (A: float, B: float, t: float) -> float:
 	return A + (B - A) * t
+	pass
+	
+func ThresholdLerp (A: float, B: float, t: float, threshold:float) -> float:
+	if(abs(A-B) <= threshold):
+		return B
+	return A + (B - A) * t
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name):
+	match anim_name:
+		"SwingL":
+			ChangeState(PlayerState.Idle)
+		"SwingR":
+			ChangeState(PlayerState.Idle)
 	pass
